18def27b78251b1fc87ab4ff23cd6a3c07bb6c84
JennyShalai <jenny.shalai@gmail.com>
2016-09-20 23:02:23 -0400

    add insert element function

--- sorted array/sorted-array.playground/Contents.swift
+++ sorted array/sorted-array.playground/Contents.swift
@@ -2,6 +2,7 @@
 
 import UIKit
 
+///////////////////////////
 
 func findElemInSortedArray(sortedArray:[Int], element: Int) -> Bool {
     
@@ -19,7 +20,70 @@ func findElemInSortedArray(sortedArray:[Int], element: Int) -> Bool {
     }
 }
 
+///////////////////////////
+
+func findElemIndex(sortedArray:[Int], element: Int) -> Int {
+    
+    if sortedArray.count == 0 {
+        return -1
+    }
+    
+    let middle = sortedArray[sortedArray.count / 2]
+    if middle == element {
+        return sortedArray.count / 2
+    } else if middle > element {
+        return findElemIndex(Array(sortedArray[0..<sortedArray.count/2]), element: element)
+    } else {
+        return findElemIndex(Array(sortedArray[sortedArray.count/2 + 1..<sortedArray.count]), element: element)
+    }
+}
+
+///////////////////////////
+
+
+func insertElement(sortedArray:[Int], element: Int) -> [Int] {
+    
+    if sortedArray.count == 0 {
+        return [element]
+    }
+    
+    var leftIndex = 0
+    var rightIndex = sortedArray.count - 1
+    var resultArray = sortedArray
+    
+    while true {
+        let middleIndex = (rightIndex + leftIndex + 1) / 2
+        let middleElem = sortedArray[middleIndex]
+        if middleElem == element {
+            return sortedArray
+        } else if middleElem > element {
+            rightIndex = middleIndex
+        } else if middleElem < element {
+            leftIndex = middleIndex
+        }
+        
+        if rightIndex - leftIndex == 1 {
+            resultArray.insert(element, atIndex: rightIndex)
+            return resultArray
+        } else if rightIndex == leftIndex {
+            if element > sortedArray[rightIndex] {
+                resultArray.insert(element, atIndex: rightIndex + 1)
+                return resultArray
+            } else if element < sortedArray[leftIndex] {
+                resultArray.insert(element, atIndex: rightIndex - 1)
+                return resultArray
+            }
+            
+        }
+        
+    }
+}
 
 print(findElemInSortedArray([1], element: 3))
+print(findElemIndex([1], element: 1))
+print(insertElement([4, 5], element: 3))
 
-[1, 2, 3, 8, 9]
+
+
+
+
