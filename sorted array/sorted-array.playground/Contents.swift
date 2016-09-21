922c41d918bf837d8fc1a4298e9b5b100edbefec
JennyShalai <jenny.shalai@gmail.com>
2016-09-20 22:12:38 -0400

    add findElement function

--- sorted array/sorted-array.playground/Contents.swift
+++ sorted array/sorted-array.playground/Contents.swift
@@ -2,4 +2,24 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+func findElemInSortedArray(sortedArray:[Int], element: Int) -> Bool {
+    
+    if sortedArray.count == 0 {
+        return false
+    }
+    
+    let middle = sortedArray[sortedArray.count / 2]
+    if middle == element {
+        return true
+    } else if middle > element {
+        return findElemInSortedArray(Array(sortedArray[0..<sortedArray.count/2]), element: element)
+    } else {
+        return findElemInSortedArray(Array(sortedArray[sortedArray.count/2 + 1..<sortedArray.count]), element: element)
+    }
+}
+
+
+print(findElemInSortedArray([1], element: 3))
+
+[1, 2, 3, 8, 9]
