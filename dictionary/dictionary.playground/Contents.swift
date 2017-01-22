0b0b28e3c761aeb9b546c79d86d5557f5fbd4c87
JennyShalai <jenny.shalai@gmail.com>
2017-01-22 13:14:19 -0500

    add main operations and loops

--- dictionary/dictionary.playground/Contents.swift
+++ dictionary/dictionary.playground/Contents.swift
@@ -28,4 +28,35 @@ dictionaryCreation["one"] = nil
 print(dictionaryCreation)   // [:]
 
 
+// main operations
+print(Array(dictionaryCreation.keys))   // ["one", "three", "two"]
+print(Array(dictionaryCreation.values)) // [1, 3, 2]
+print(dictionaryCreation.isEmpty)       // true
 
+dictionaryCreation = ["one": 1, "two": 2, "three": 3]
+
+
+
+
+// loops - data in dict is not ordered so it will print out key-value pairs randomly every time we looping through dictionary
+
+for (string, number) in dictionaryCreation {
+    print("key: \(string), value:\(number)")
+}
+// key: one, value:1
+// key: three, value:3
+// key: two, value:2
+
+for key in dictionaryCreation.keys {
+    print(key)
+}
+// one
+// three
+// two
+
+for value in dictionaryCreation.values {
+    print(value)
+}
+// 1
+// 3
+// 2
